6a87b2e179b5e4d2d8f5a8853c02d88f2abc5e7e
GozCorrea <62217597+GozCorrea@users.noreply.github.com>
2020-04-22 18:40:23 -0300

    Elementos ataque
    Clase elementos ataque

---
+++ src/Library/ElementosAtaque.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Library
+{
+    public class ElementosAtaque
+    {
+        //potencia pude 1, 2 o 3.
+        //si es 
+        //potencia alta disminuye la vida en 3, media en 2 y baja en 1
+        public ElementosAtaque(string descripcion, int potencia, int costo)
+        {
+            this.Descripcion = descripcion;
+            this.Potencia = potencia;
+            this.Costo = costo;
+        }
+
+        public String Descripcion { get; set; }
+        public int Potencia { get; set; }
+        public int Costo { get; set; }
+    }
+}
